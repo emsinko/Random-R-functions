6d4c9d5dfea4bc3acfb5717b9daf82469c74c50f
emsinko <44008559+emsinko@users.noreply.github.com>
2019-03-13 10:21:33 +0100

    Update MatrixPowers.R

--- MatrixPowers.R
+++ MatrixPowers.R
@@ -1,9 +1,9 @@
-mocnina <- function(mat,x){
+mocnina <- function(mat,power){
   A <- mat
-  if(x == 0) return(diag(dim(A)[1]))
-  else if(x == 1) return(mat) else
-    for(i in 1:(x-1)){ 
-      A <- A%*%mat
+  if(power == 0) return(diag(dim(A)[1]))
+  else if(power == 1) return(mat) else
+    for(i in 1:(power-1)){ 
+      A <- A %*% mat
     }
   return(A)
 }
