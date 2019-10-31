998ea4265eb63347a755b95a082711d34e7046c3
emsinko <44008559+emsinko@users.noreply.github.com>
2019-10-31 21:07:30 +0100

    Add files via upload

---
+++ SlovnaftBajk.R
@@ -0,0 +1,27 @@
+library(leaflet)
+library(tidyverse)
+
+# https://rstudio.github.io/leaflet/
+# https://rstudio.github.io/leaflet/map_widget.html
+
+data <- read.delim('data.csv',sep= ';')
+data <- read.delim("https://opendata.bratislava.sk/dataset/download/411",sep= ';')  # priamo z webu
+head(data)
+
+colnames(data) <- c("ID","nazov", "GPSlat","GPSlon")
+
+
+mapa <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng = data$GPSlon, lat = data$GPSlat, label = data$nazov, popup = data$nazov)
+  #addCircles(lat = data$GPSlat, lng = data$GPSlon)
+  #addCircleMarkers(radius = 7, fill = TRUE, lat = data$GPSlat, lng = data$GPSlon)
+
+print(mapa)
+
+# Ciernobiela theme
+leaflet() %>% 
+  addProviderTiles(providers$Stamen.Toner) %>%
+  addCircleMarkers(radius = 7, fill = TRUE, lat = data$GPSlat, lng = data$GPSlon)
+
+
